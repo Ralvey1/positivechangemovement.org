d4ca67b30609e31bf7721323a4b1b0f8219386e6
Ryan Alvey <102349808+Ralvey1@users.noreply.github.com>
2025-01-08 13:09:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,10 @@ document.addEventListener("DOMContentLoaded", function() {
 document.getElementById("myButton").addEventListener("click", function () {
     alert("Hello from External JavaScript!");
 });
+<script>
+    document.querySelectorAll("nav ul li a").forEach(link => {
+        if (link.href === window.location.href) {
+            link.classList.add("active");
+        }
+    });
+</script>
